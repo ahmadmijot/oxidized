5165de12cdc4a34cd16914742470dce4a71eb65e
ahmd mijot <ahmad.mijot@gmail.com>
2020-12-06 20:24:59 +0800

    Update main.rs

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(5, 6);
+
+    let x = 5;
+    let y = {
+        let x = 3;
+        x + 1 //why x value can change?
+    };
+
+    println!("The value of \n x is: {} \n y is: {}", x, y);
 }
 
-fn another_function() {
-    println!("Another function");
+fn another_function(i: i32, j: i32) {
+    println!("The value of \n i is: {} \n j is: {}", i,j);
 }
