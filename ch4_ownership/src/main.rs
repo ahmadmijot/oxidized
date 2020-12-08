16c2ac0e468c23962d140176139096cb85ffd6aa
ahmd mijot <ahmad.mijot@gmail.com>
2020-12-09 01:10:01 +0800

    edit ch4

--- ch4_ownership/src/main.rs
+++ ch4_ownership/src/main.rs
@@ -1,10 +1,25 @@
 fn main() {
-    string_type();
+   let s = String::from("hello"); // s comes into scope
+
+   takes_ownership(s);
+
+   let x = 5;
+   makes_copy(x);
 }
 
-fn string_type() {
-    let s = String::from("This is tring literal");
-    let mut t = String::from("This can be mutated");
-    t.push_str("like this");
-    println!("{},{}",s, t);
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
 }
+
+fn makes_copy(some_int: i32) {
+    println!("{}", some_int);
+}
+
+
+// fn string_type() {
+//     let s = String::from("This is string literal \n");
+//     let mut t = String::from("This can be mutated \n");
+//     t.push_str("like this");
+//     let r = t;
+//     println!("{},{}",r, s);
+// }
